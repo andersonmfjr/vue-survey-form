cf4cfabbdeaa02c2d8940940a05f450b1bf12d9b
Anderson Jr <andersonmfjr@gmail.com>
2019-07-28 18:09:13 -0300

    :rocket: Add config for use icons

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
+import VeeValidate from 'vee-validate';
+import VueAwesome from 'vue-awesome';
+import VFC from 'vfc';
+import 'vfc/dist/vfc.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store/index';
 
-import FontAwesome from './assets/fontawesome';
-
-Vue.use('fa-icon', FontAwesome);
+Vue.use(VeeValidate);
+Vue.component('v-icon', VueAwesome);
+Vue.use(VFC);
 
 Vue.config.productionTip = false;
 
