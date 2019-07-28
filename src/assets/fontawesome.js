909fc6c41818c1336574467db1f7cd0c50cdc16d
Anderson Jr <andersonmfjr@gmail.com>
2019-07-28 17:38:40 -0300

    :fire: Delete fontawesome

--- src/assets/fontawesome.js
+++
@@ -1,8 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-library.add(faChevronUp);
-library.add(faChevronDown);
-
-export default FontAwesomeIcon;
