c0f351c721008699df1c509713ad95df610a1d19
Anderson Jr <andersonmfjr@gmail.com>
2019-07-27 22:08:47 -0300

    :rocket: Add home page

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">
-        Home
-      </router-link> |
-      <router-link to="/about">
-        About
-      </router-link>
-    </div>
+    <the-header />
     <router-view />
   </div>
 </template>
 
+<script>
+import TheHeader from '@/components/TheHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
 <style lang="less">
-#app {
+html {
+  font-size: 10px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
   text-align: center;
   color: #2c3e50;
 }
+
 #nav {
   padding: 30px;
   a {
