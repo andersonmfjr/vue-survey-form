5ec92b1e5abbbc35862408219395619cf260375f
Anderson Jr <andersonmfjr@gmail.com>
2019-08-03 21:56:47 -0300

    :rocket: Update vuex with fetch data status

--- src/store/form/actions.js
+++ src/store/form/actions.js
@@ -1,17 +1,22 @@
 import { db } from '@/config/db';
-import { SET_FORM_DATA, UPDATE_FORM_STATE } from './types';
+import {
+  SET_FORM_DATA, UPDATE_FORM_STATE, FETCH_START, FETCH_SUCCESS, FETCH_ERROR,
+} from './types';
 
 export default {
   async fetchForm({ commit }) {
     try {
+      commit(FETCH_START);
       const ref = db.collection('questions');
       const response = await ref.orderBy('order', 'asc').get();
       const questions = response.docs.map(doc => ({ ...doc.data(), id: doc.id }));
 
       commit(SET_FORM_DATA, questions);
       commit(UPDATE_FORM_STATE, { key: 'formLength', value: questions.length });
+      commit(FETCH_SUCCESS);
       return true;
     } catch (error) {
+      commit(FETCH_ERROR);
       return error;
     }
   },
--- src/store/form/mutations.js
+++ src/store/form/mutations.js
@@ -1,4 +1,11 @@
-import { SET_FORM_DATA, UPDATE_FORM_STATE, UPDATE_FORM_DATA } from './types';
+import {
+  SET_FORM_DATA,
+  UPDATE_FORM_STATE,
+  UPDATE_FORM_DATA,
+  FETCH_ERROR,
+  FETCH_START,
+  FETCH_SUCCESS,
+} from './types';
 
 export default {
   [SET_FORM_DATA](state, payload) {
@@ -9,5 +16,16 @@ export default {
   },
   [UPDATE_FORM_DATA](state, payload) {
     state.formData[payload.key] = payload.value;
+  },
+  [FETCH_START](state) {
+    state.fetching = true;
+  },
+  [FETCH_SUCCESS](state) {
+    state.fetching = false;
+    state.fetchSuccess = true;
+  },
+  [FETCH_ERROR](state) {
+    state.fetching = false;
+    state.fetchError = true;
   },
 };
